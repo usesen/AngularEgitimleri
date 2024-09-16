20900a06800a4d6097e1431df1e18942f40e05a9
Uğur Şeşen <58859424+usesen@users.noreply.github.com>
2024-09-16 13:22:40 +0300

    Angular Proje yaratma,npm yüklemesi, menüler,ders notları,dizinlerin yaratılması,componentlerin yaratılması,routing ayarlarının yapılması,quard giriş

--- src/app/features/customer-management/components/new-customer-card/new-customer-card.component.ts
+++ src/app/features/customer-management/components/new-customer-card/new-customer-card.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Customer {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  address: string;
+  city: string;
+  country: string;
+  postalCode: string;
+  company: string;
+  position: string;
+  notes: string;
+}
 
 @Component({
   selector: 'app-new-customer-card',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-customer-card.component.html',
   styleUrl: './new-customer-card.component.scss'
 })
 export class NewCustomerCardComponent {
+  customer: Customer = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    address: '',
+    city: '',
+    country: '',
+    postalCode: '',
+    company: '',
+    position: '',
+    notes: ''
+  };
 
+  saveCustomer() {
+    console.log('Müşteri kaydedildi:', this.customer);
+    // Burada müşteriyi kaydetme işlemi yapılacak
+  }
 }
